83e33cd1612887d8c49a7679bca6b4467384b566
MatiasGobbi <migobbi555@gmail.com>
2024-10-18 16:56:00 -0300

    Conflicto

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Header from "@/components/Header/Header";
+import Header from "@/components/Header";
 import Hero from "@/components/Hero/Hero";
 import axios from "axios";
 
