d5860f75d1d8db84520a207ed1bba7384f472040
MatiasGobbi <migobbi555@gmail.com>
2024-10-23 20:48:19 -0300

    Banner finished

--- components/sections/Banner.tsx
+++ components/sections/Banner.tsx
@@ -2,14 +2,11 @@ import CustomButton from '../CustomButton'
 const Banner = () => {
     return (
         <section className='py-[80px]'>
-            <div className="jtm-container">
-                <h2>
-                    Titulo de pagina Version Psicologo - Con VSL
+            <div className="jtm-container text-center bg-pink-200 py-[80px] px-[123px]">
+                <h2 className='text-[32px] font-bold text-gray-800 max-w-[499px] mx-auto leading-[120%]'>
+                    Call to action final ej Agenda ahora y no pierdas más
                 </h2>
-                <p>
-                    Descripción del título
-                </p>
-                <CustomButton/>
+                <CustomButton customClasses='mt-[32px] mx-auto'/>
             </div>
         </section>
     )
