25617afea8664e8a7c7133c2eac6cfe68f6b0b83
tomas <tomascostapp@gmail.com>
2024-10-30 23:23:50 -0300

    Detalles

--- app/components/sections/Banner.tsx
+++ app/components/sections/Banner.tsx
@@ -2,8 +2,8 @@ import CustomButton from '@/app/components/CustomButton'
 
 export default function Banner() {
   return (
-    <section className="py-[80px]">
-      <div className="jtm-container text-center bg-white py-[80px]">
+    <section className="py-[80px] px-4">
+      <div className="jtm-container text-center bg-white py-[80px] rounded-3">
         <h2 className="text-[32px] font-bold text-gray-800 max-w-[900px] mx-auto leading-[120%]">
           No le regales tu dinero a "profesionales" ni "agencias" que te cobran
           400 usd por una web que parece y en muchos casos es una plantilla
